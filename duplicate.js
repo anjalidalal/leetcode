f5444655b0043ea1051a7b9b9197a6c55acbce2b
anjalidalal <anjalimaria2002@gmail.com>
2022-03-27 16:15:57 +0530

    return the repeated number

--- duplicate.js
+++ duplicate.js
@@ -10,7 +10,7 @@
 // Share
 // Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
-// There is only one repeated number in nums, return this repeated number.
+// There is only one repeated number in nums, return the repeated number.
 
 // You must solve the problem without modifying the array nums and uses only constant extra space.
 
