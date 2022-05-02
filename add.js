ef321e8f307424211ed21e90da6e0f7f50ce6371
anjalidalal <anjalimaria2002@gmail.com>
2022-05-02 16:17:05 +0530

    Given two integers num1 and num2, return the sum of the two integers

---
+++ add.js
@@ -0,0 +1,45 @@
+// 2235. Add Two Integers
+// Easy
+
+// 116
+
+// 474
+
+// Add to List
+
+// Share
+// Given two integers num1 and num2, return the sum of the two integers.
+ 
+
+// Example 1:
+
+// Input: num1 = 12, num2 = 5
+// Output: 17
+// Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+// Example 2:
+
+// Input: num1 = -10, num2 = 4
+// Output: -6
+// Explanation: num1 + num2 = -6, so -6 is returned.
+ 
+
+// Constraints:
+
+// -100 <= num1, num2 <= 100
+// Accepted
+// 16,169
+// Submissions
+// 17,342
+// Seen this question in a real interview before?
+
+// Yes
+
+// No
+
+
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
+
+let num1 = 12, num2 = 5;
+console.log(sum(num1, num2))
